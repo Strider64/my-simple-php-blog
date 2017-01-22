ed2d8fcd24e4268648fa8353c2bc1582dfc75860
John Pepp <jrpepp@pepster.com>
2017-01-22 07:59:11 -0500

    Add files via upload

--- lib/includes/config.php
+++ lib/includes/config.php
@@ -1,4 +1,14 @@
 <?php
+
+/* Turn on error reporting */
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+if (filter_input(INPUT_SERVER, 'SERVER_NAME', FILTER_SANITIZE_URL) == "localhost") {
+    error_reporting(-1); // -1 = on || 0 = off
+} else {
+    error_reporting(0); // -1 = on || 0 = off
+}
+
 include 'connect/mySimpleDBconnect.php';
 /*
  * Pepster's Place 
@@ -18,39 +28,41 @@ session_start();
 // Check for a user in the session:
 $user = (isset($_SESSION["user"])) ? $_SESSION["user"] : NULL;
 
-try {
-    $conn = new PDO("mysql:host=localhost:8889;dbname=mysimpleblog", DATABASE_USERNAME, DATABASE_PASSWORD);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+function createTables() {
+    try {
+        $conn = new PDO("mysql:host=localhost:8889;dbname=mysimpleblog", DATABASE_USERNAME, DATABASE_PASSWORD);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $table1 = "CREATE TABLE IF NOT EXISTS mysimpleblog (
+        $table1 = "CREATE TABLE IF NOT EXISTS mysimpleblog (
                 id INT(11) AUTO_INCREMENT PRIMARY KEY,
                 userid INT(11) NOT NULL,
                 name VARCHAR(60) NOT NULL,
                 title VARCHAR(60) NOT NULL,
                 message TEXT NOT NULL,
                 dateCreated DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00')";
-    $conn->exec($table1);
-    $use = 'use mysimpleblog';
-    $conn->exec($use);
-    $table2 = "CREATE TABLE IF NOT EXISTS mysimpleregistration (
+        $conn->exec($table1);
+        $use = 'use mysimpleblog';
+        $conn->exec($use);
+        $table2 = "CREATE TABLE IF NOT EXISTS mysimpleregistration (
                 id INT(11) AUTO_INCREMENT PRIMARY KEY,
                 name VARCHAR(60) NOT NULL,
                 email VARCHAR(120) NOT NULL,
                 password VARCHAR(255) NOT NULL,
                 dateCreated DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00')";
-    $conn->exec($table2);
-    $conn = NULL;
-    
-} catch (PDOException $e) {
-    echo "Something went wrong" . $e->getMessage();
+        $conn->exec($table2);
+        $conn = NULL;
+    } catch (PDOException $e) {
+        echo "Something went wrong" . $e->getMessage();
+    }
 }
 
-$db_options = array(
-    /* important! use actual prepared statements (default: emulate prepared statements) */
-    PDO::ATTR_EMULATE_PREPARES => false
-    /* throw exceptions on errors (default: stay silent) */
-    , PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    /* fetch associative arrays (default: mixed arrays)    */
-    , PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-);
-$pdo = new PDO('mysql:host=' . DATABASE_HOST . ';dbname=' . DATABASE_NAME . ';charset=utf8', DATABASE_USERNAME, DATABASE_PASSWORD, $db_options);
+createTables();
+
+
+/* Autoloads classes using namespaces                       */
+require_once "lib/website_project/website_project.inc.php";
+
+use website_project\database\Database as DB;
+
+$db = DB::getInstance();
+$pdo = $db->getConnection();
