f9633b015899bbe5d6ab0748933c144947172541
John Pepp <jrpepp@pepster.com>
2017-01-23 17:48:30 -0500

    Add files via upload

--- lib/includes/config.php
+++ lib/includes/config.php
@@ -25,44 +25,22 @@ $days = 14;
 session_set_cookie_params($seconds * $minutes * $hours * $days, "/", "", true, true);
 session_start();
 
-// Check for a user in the session:
-$user = (isset($_SESSION["user"])) ? $_SESSION["user"] : NULL;
-
-function createTables() {
-    try {
-        $conn = new PDO("mysql:host=localhost:8889;dbname=mysimpleblog", DATABASE_USERNAME, DATABASE_PASSWORD);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        $table1 = "CREATE TABLE IF NOT EXISTS mysimpleblog (
-                id INT(11) AUTO_INCREMENT PRIMARY KEY,
-                userid INT(11) NOT NULL,
-                name VARCHAR(60) NOT NULL,
-                title VARCHAR(60) NOT NULL,
-                message TEXT NOT NULL,
-                dateCreated DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00')";
-        $conn->exec($table1);
-        $use = 'use mysimpleblog';
-        $conn->exec($use);
-        $table2 = "CREATE TABLE IF NOT EXISTS mysimpleregistration (
-                id INT(11) AUTO_INCREMENT PRIMARY KEY,
-                name VARCHAR(60) NOT NULL,
-                email VARCHAR(120) NOT NULL,
-                password VARCHAR(255) NOT NULL,
-                dateCreated DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00')";
-        $conn->exec($table2);
-        $conn = NULL;
-    } catch (PDOException $e) {
-        echo "Something went wrong" . $e->getMessage();
-    }
-}
-
-createTables();
-
-
 /* Autoloads classes using namespaces                       */
 require_once "lib/website_project/website_project.inc.php";
 
+/* Function folder of important and useful functions */
+include 'lib/functions/functions.inc.php';
+
 use website_project\database\Database as DB;
 
+// Check for a user in the session:
+$user = (isset($_SESSION["user"])) ? $_SESSION["user"] : NULL;
+
+
+createTables(); // Create database tables if necessary:
+
+/*
+ * Create a constant PDO connection.
+ */
 $db = DB::getInstance();
 $pdo = $db->getConnection();
